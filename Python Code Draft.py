be11d74c03caaeb32b5595532b5498789b08c793
MStoreide <markus.storeide@gmail.com>
2022-08-29 13:34:26 -0400

    Added object complexity draft

--- Python Code Draft.py
+++ Python Code Draft.py
@@ -1,13 +1,18 @@
 #Rambling pseudocode for the metric
 
-# import best 3D toolbox as ?? (PyMesh?)
+# import pymesh as pym
 # import numpy as np 
 # import pyplot as plt
 
-# mesh = pymesh.load_mesh(3Dfile) -- Must be a mesh --Prints general info about the mesh
-# print("Filename", \n"ObjectName", \n"Dimensions") --Dimensions must be taken from some metadata. 
+# mesh = pym.load_mesh(3Dfile) -- Must be a mesh. Prints general info about the mesh
+# print("Filename", \n"ObjectName", \n"Dimensions") --Dimensions must be taken from some metadata.
+# 
+# print("Fileinfo: ", \n"Vertices: ", vertex.amount, \n"Polygons: ", poly.amount)
+# 
+# def object.complexity(3Dfile) --Will give a general impression of the complexity of the geometry
+#   bregman divergence for the distance?
 
-# function vertex.variance(3Dfile) --Will calculate the variance in vertices
+# def vertex.variance(3Dfile) --Will calculate the variance in vertices
 #   for vertex in 3Dfile
 #       calculate min, max, and mean xyz difference
 # print("X variance: ", x.var, \n"Y variance: ", y.var, \n"Z variance: ", z.var)
@@ -16,8 +21,9 @@
 # plt.plot(y.vertex.values)
 # plt.plot(z.vertex.values)
 
-# function polygon.variance(3Dfile) --Will calculate the variance in polygons
+# def polygon.variance(3Dfile) --Will calculate the size variance in polygons
 #   for ????
 #       ?? 
+# poly.sizes = Array(poly size range)
 # print("Min poly size: ", min.poly.size, \n"Max poly size: ", max.poly.size, \n"Mean poly size: ", mean.poly.size)
 # plt.plot(poly.sizes)
